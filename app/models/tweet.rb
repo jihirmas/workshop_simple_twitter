9d04d86633331fe3ffc3e2504d73635389964db2
jihirmas <jihirmas@miuandes.cl>
2023-06-10 15:48:17 -0400

    tweets

---
+++ app/models/tweet.rb
@@ -0,0 +1,8 @@
+class Tweet < ApplicationRecord
+    belongs_to :user
+
+    def creation_date
+        created_at.strftime("%e %B %Y")
+
+    end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+5.times do |i|
+    User.create!(
+      first_name: "First Name #{i+1}",
+      last_name: "Last Name #{i+1}",
+      email: "user#{i+1}@example.com",
+      password: "password"
+    )
+  end
+  
+  # Crear tweets para cada usuario
+  User.all.each do |user|
+    5.times do |i|
+      user.tweets.create!(
+        body: "Tweet information blabla  blalbalbalbalb asdasd#{i+1}"
+      )
+    end
+  end
